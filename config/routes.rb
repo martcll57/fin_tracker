d1e1fcf639c45b542677a738dfac5bd6f07802d9
Martin <martcll57p@gmail.com>
2018-01-21 06:52:20 +0000

    add user and stock associations and views

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,7 @@ Rails.application.routes.draw do
   root 'welcome#index'
   get 'my_portfolio', to: "users#my_portfolio"
   get 'search_stocks', to: 'stocks#search'
-  
+  resources :user_stocks, only: [:create, :destroy]
   
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
