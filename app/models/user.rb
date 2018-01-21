e296891b17317b6ee43305e17eb016ed0fa7f002
Martin <martcll57p@gmail.com>
2018-01-21 16:07:23 +0000

    add and remove friends and show page view

--- app/models/user.rb
+++ app/models/user.rb
@@ -28,4 +28,37 @@ class User < ActiveRecord::Base
     user_stocks.where(stock_id: stock.id).exists?
   end
   
+  def not_friends_with?(friend_id)
+    friendships.where(friend_id: friend_id).count < 1
+  end
+  
+  def except_current_users(users)
+    users.reject { |user| user.id == self.id }
+  end
+  
+  def self.search(param)
+    param.strip!
+    param.downcase!
+    to_send_back = (first_name_matches(param) + last_name_matches(param) + email_matches(param)).uniq
+    return nil unless to_send_back
+    to_send_back
+  end
+  
+  def self.first_name_matches(param)
+    matches('first_name', param)
+  end
+  
+  def self.last_name_matches(param)
+    matches('last_name', param)
+  end
+  
+  def self.email_matches(param)
+    matches('email', param)
+  end
+  
+  def self.matches(field_name, param)
+    where("lower(#{field_name}) like ?", "%#{param}%")
+  end
+  
+  
 end
